be44ac6f83e2a3b0f060c049edc7cdf4b0097396
AshanaShaikh <ashanashaikh0204@gmail.com>
2026-09-14 14:51:43 +0530

    feat: add role-based get tickets API

--- backend/src/routes/ticketRoutes.js
+++ backend/src/routes/ticketRoutes.js
@@ -64,4 +64,50 @@ router.post("/", authenticateToken, async (req, res) => {
         });
     }
 });
+
+//Role Based Authentication
+router.get("/", authenticateToken, async (req, res) => {
+    try {
+        const userId = req.user.id;
+        const role = req.user.role;
+
+        let tickets;
+        //Admin - All tickets
+        if (role === "Admin") {
+            tickets = await pool.query(
+                "SELECT * FROM tickets ORDER BY created_at DESC"
+            );
+        }
+        //User - Tickets created by the user
+        else if (role === "User") {
+            tickets = await pool.query(
+                "SELECT * FROM tickets WHERE created_by = $1 ORDER BY created_at DESC",
+                [userId]
+            );    
+        } 
+        //Agent - Roles assigned to the Agent
+        else if (role === "Agent") { 
+            tickets = await pool.query(
+                "SELECT * FROM tickets WHERE assigned_to = $1 ORDER BY created_at DESC",
+                [userId]
+            );
+        } 
+        //Invalid role - Access denied
+        else {
+            return res.status(403).json({
+                message: "Access denied"
+            });
+        }
+
+        res.status(200).json({
+        tickets: tickets.rows
+        });
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            message: "Internal server error"
+        });
+    }
+});
 module.exports = router;
